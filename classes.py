db08cc425e45a60fa6a975aca1d6d5dd1501913f
Taffiemagie <cameron.wmacd@gmail.com>
2026-07-23 10:47:08 -0400

    Fixed removal of cards in for loop and added attributes to player menu

--- classes.py
+++ classes.py
@@ -4,12 +4,17 @@ import random
 
 #Class representing one playing card
 class card:
+    face_cards = {0:"Joker", 1:"Ace", 11:"Jack", 12:"Queen", 13:"King"}
     def __init__(self,suit,value):
         self.suit = suit
         self.value = value
-        self.uname = f"{self.value} of {self.suit}"
+        #Below refers to the above dictionary and changes the user friendly name to the face card
+        if self.value in self.face_cards:
+            self.uname = f"{card.face_cards[self.value]} of {self.suit}"
+        else:
+            self.uname = f"{self.value} of {self.suit}"
 
-#Class representing a deck of 52 standard playing cards
+#Class representing a deck of 52 standard playing cards (No Jokers)
 class deck_52:
     suits = ["Spades","Clubs","Hearts","Diamonds"]
     values = [1,2,3,4,5,6,7,8,9,10,11,12,13]
@@ -19,6 +24,7 @@ class deck_52:
     
     #Goes through the list of suits and values to add all the cards to the deck
     def new(self):
+        self.card_list = []
         for suit in deck_52.suits:
             for value in deck_52.values:
                 self.card_list.append(card(suit,value))
@@ -32,6 +38,6 @@ class deck_52:
     def shuffle_deck(self):
         random.shuffle(self.card_list)
 
-    #Pulls top card from deck
+    #Pulls top card from deck (Last card in the list)
     def draw(self):
-        return self.card_list.pop(0)
+        return self.card_list.pop()
--- main.py
+++ main.py
@@ -10,30 +10,54 @@ import classes
 room = []
 hp = 0
 scoundrel_deck = classes.deck_52()
+#Weapon is a list: the card at index 0 will be the weapon, the rest will be monsters.
+weapon = ["None"]
 
 #Sets the variables for a new game then shuffles the deck and creates the room
-def new_game(deck):
+def new_game():
     global room
     global hp
     global scoundrel_deck
-    hp = 10
+    global weapon
+    room = []
+    hp = 20
+    weapon = ["None"]
     scoundrel_deck.new()
+    #Marks the list index for removal, then removes it from the for loop. Removing during the for loop causes an out of bounds error.
+    tmp_toremove = []
+    for card in scoundrel_deck.card_list:
+        if card.value == 1:
+            tmp_toremove.append(card)
+        elif card.value >=11 and (card.suit == "Diamonds" or card.suit == "Hearts"):
+            tmp_toremove.append(card)
+    for card in tmp_toremove:
+        scoundrel_deck.card_list.remove(card)
     scoundrel_deck.shuffle_deck()
     for i in range(4):
-        room.append(deck.draw())
+        room.append(scoundrel_deck.draw())
 
 #Shows the appropriate menu according to cards in the room
 def show_menu():
     global room
-    print("~ Select a card from below or press \'help\' ~")
+    global hp
+    global scoundrel_deck
+    global weapon
+    if len(weapon) == 1:
+        print(f"~ HP:{hp} Cards left:{len(scoundrel_deck.card_list)} ~\n~ Weapon:{weapon[0]} ~\n~ Select a card from below or press \'help\' ~")
+    elif len(weapon) > 1:
+        print(f"~ HP:{hp} Cards left:{len(scoundrel_deck.card_list)} ~\n~ Weapon:{weapon[0]} Slain monster:{weapon[-1]} ~\n~ Select a card from below or press \'help\' ~")
     card_num = 1
     for card in room:
         print(f"{card_num}: {card.uname}")
         card_num += 1
 
+#Allows the user to select a option and updates global variables
+def play_card():
+    global room
+
 #Gameplay loop.
 def __main__():
-    new_game(scoundrel_deck)
+    new_game()
     show_menu()
 
 __main__()
